d1972fed80277000d8d21c9bb8c460541544ba88
godwillcodes <iamgodwillb@gmail.com>
2024-05-28 02:25:30 +0300

    initial setup

---
+++ resources/views/livewire/page-banner.blade.php
@@ -0,0 +1,29 @@
+<div class="bg-gray-900">
+    <header class="absolute inset-x-0 top-0 z-40">
+        {{-- navigation here --}}
+        @livewire('header-navigation')
+    </header>
+    <div class="relative isolate overflow-hidden pt-14 lg:px-0 px-10">
+        <!-- Background Image with Overlay -->
+        <div class="absolute inset-0">
+            <img src="/images/banner.jpeg" alt="" class="h-full w-full object-cover">
+            <div class="absolute inset-0 bg-black opacity-70"></div>
+        </div>
+
+        <!-- Content Container -->
+        <div class="relative mx-auto max-w-7xl py-16 sm:py-24 lg:pt-44">
+            <!-- Newsroom Heading -->
+            
+
+            <h1 data-aos="fade-left" data-aos-duration="1500" class="text-6xl my-6 font-bold text-white lg:py-0 py-5">Reducing Emissions<br>
+                Improving Lives</h1>
+
+            <p class="text-white max-w-2xl pb-5">Smart, efficient, and sustainable fuel usage made easy! Join Pika na Power for an electrifying culinary journey today!</p>
+
+            
+            
+
+            
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div>
+        <livewire:page-banner/>
+    </div>
+</x-app-layout>
