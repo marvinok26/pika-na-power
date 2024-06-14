806a0d9570b7c462a6a261aedd0e8e19816278b8
Makena-Maryann <maryann.makena00@gmail.com>
2024-06-14 11:24:55 +0300

    feat: Add Banner model, migration, seeder, and Livewire component

---
+++ database/seeders/BannerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sections\Banner;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class BannerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Banner::create([
+            'title' => 'Reducing Emissions,Improving Lives',
+            'description' => 'Smart, efficient, and sustainable fuel usage made easy! Join Pika na Power for an electrifying culinary journey today!',
+            'image' => '/images/banner.jpeg',
+            'page_section_id' => 1,
+        ]);
+    }
+}
