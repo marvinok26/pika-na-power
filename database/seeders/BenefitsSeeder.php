5983365ef71767b1283e20ccdad2d6c1d0e7d9a4
Makena-Maryann <maryann.makena00@gmail.com>
2024-06-18 13:31:07 +0300

    feat: Add Benefits model, migration, seeder, and Livewire component

---
+++ database/seeders/BenefitsSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\content\Benefit;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class BenefitsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Benefit::create([
+            'title' => 'Energy Efficiency',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Cleaner Cooking',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Environmental Sustainability',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Health Benefits',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Safety',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Reduced Workload',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Cultural Acceptance and Modernization',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Capacity Building',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Equitable Access',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+        Benefit::create([
+            'title' => 'Global Sustainable Development',
+            'description' => 'Mollit duis ex sit Lorem aute reprehenderit labore aliqua proident magna ex commodo tempor. Pariatur ea tempor mollit laborum ut qui amet enim ea labore aute. Pariatur magna velit velit. Ad elit incididunt esse nulla adipisicing labore cupidatat laborum enim ex.',
+        ]);
+    }
+}
