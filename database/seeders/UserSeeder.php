a19f1ec63c4d5e832f141731ac3748c3d3272a02
Makena-Maryann <maryann.makena00@gmail.com>
2024-06-24 09:25:03 +0300

    refactor: Update user model to implement FilamentUser and add canAccessPanel method

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
             'name' => 'Belva Admin',
             'email' => 'dev@belvadigital.com',
             'password' => bcrypt('12345678!'),
+            'email_verified_at' => now(),
         ]);
     }
 }
