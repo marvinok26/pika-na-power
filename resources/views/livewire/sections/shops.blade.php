6293073112040bc7685deba72bf2c7591fe27316
kigathi-chege <chegekigathi@gmail.com>
2024-07-26 11:18:07 +0300

    feat: introduce aos animations

--- resources/views/livewire/sections/shops.blade.php
+++ resources/views/livewire/sections/shops.blade.php
@@ -1,23 +1,25 @@
 @if ($sectiondata != null)
-    <section class="bg-white dark:bg-slate-950 bg-top bg-no-repeat py-12" >
+    <section class="bg-white dark:bg-slate-950 bg-top bg-no-repeat py-12">
         <div class="mx-auto container pb-10">
             <div class="flex flex-wrap justify-center items-center -mx-4">
                 <!-- Video Column -->
                 <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0">
                     <div class="text-3xl dark:text-white text-primary lg:text-5xl my-6 font-medium font-serif lg:py-0">
-                        {{$sectiondata->title}}
+                        {{ $sectiondata->title }}
                     </div>
                 </div>
                 <!-- Text Column -->
                 <div class="w-full md:w-1/2 px-4">
-                    <div class="dark:text-white font-sans text-black text-base">{!!$sectiondata->description!!}</div>
+                    <div class="dark:text-white font-sans text-black text-base">{!! $sectiondata->description !!}</div>
                 </div>
             </div>
             <div class="grid mt-8 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 <!-- Item 1 -->
                 @foreach ($shops as $shop)
-                    <a href={{$shop->url}} class="border-2 rounded-xl dark:border-black border-sky-300 text-center bg-gray-50 p-6 hover:border-blue-800">
-                        <img src={{ url('storage/'.$shop->logo) }} class="h-28 mx-auto text-center object-contain" alt="">
+                    <a data-aos="flip-right" href={{ $shop->url }}
+                        class="border-2 rounded-xl dark:border-black border-sky-300 text-center bg-gray-50 p-6 hover:border-blue-800">
+                        <img src={{ url('storage/' . $shop->logo) }} class="h-28 mx-auto text-center object-contain"
+                            alt="">
                     </a>
                 @endforeach
             </div>
--- resources/views/livewire/sections/why-us.blade.php
+++ resources/views/livewire/sections/why-us.blade.php
@@ -1,22 +1,25 @@
 @if ($sectiondata != null)
-    <section class="bg-top bg-no-repeat w-full bg-lightblue dark:bg-slate-950 py-12 bg-cover" style="background-image: url('{{ asset('/images/boxbackground.svg') }}');">
+    <section class="bg-top bg-no-repeat w-full bg-lightblue dark:bg-slate-950 py-12 bg-cover"
+        style="background-image: url('{{ asset('/images/boxbackground.svg') }}');">
         <div class="mx-auto container pb-10">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-20 items-center">
                 <div class="flex flex-col justify-center gap-6">
-                    <div  class="text-3xl font-serif text-primary lg:text-5xl lg:py-0">
-                        {{$sectiondata->title}}
-                    </div>                
-                    <div class="font-sans leading-7 text-base text-black">{!!$sectiondata->description!!}</div>
+                    <div data-aos="fade-left" class="text-3xl font-serif text-primary lg:text-5xl lg:py-0">
+                        {{ $sectiondata->title }}
+                    </div>
+                    <div data-aos="fade-left" class="font-sans leading-7 text-base text-black">{!! $sectiondata->description !!}
+                    </div>
                 </div>
                 <div class="flex justify-center">
                     <div class="relative">
-                        <img src={{ url('storage/'.$sectiondata->image) }} class="w-full min-h-96 object-cover rounded-lg" alt="...">
+                        <img data-aos="flip-right" src={{ url('storage/' . $sectiondata->image) }}
+                            class="w-full min-h-96 object-cover rounded-lg" alt="...">
                     </div>
                 </div>
             </div>
         </div>
         @if ($benefits != null)
-            <livewire:content.benefits :benefits="$benefits"/>
+            <livewire:content.benefits :benefits="$benefits" />
         @endif
     </section>
 @endif
