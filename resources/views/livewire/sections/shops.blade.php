446d846ecc0785469a66ad163fcc2330d0d29455
Makena-Maryann <maryann.makena00@gmail.com>
2024-08-15 10:55:06 +0300

    Refactor partner and shop sections to use anchor tags for external links

--- resources/views/livewire/sections/shops.blade.php
+++ resources/views/livewire/sections/shops.blade.php
@@ -17,7 +17,7 @@
                 <div class="grid mt-8 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     <!-- Item 1 -->
                     @foreach ($shops as $shop)
-                        <a data-aos="flip-right" href={{ $shop->url }}  target="_blank" rel="noreferrer" class="border-2 rounded-xl dark:border-black border-sky-300 text-center bg-gray-50 p-6 hover:border-blue-800">
+                        <a data-aos="flip-right" href="{{ $shop->url }}"  target="_blank" rel="noreferrer" class="border-2 rounded-xl dark:border-black border-sky-300 text-center bg-gray-50 p-6 hover:border-blue-800">
                             <img src={{ url('storage/' . $shop->logo) }} class="h-28 mx-auto text-center object-contain" alt="">
                         </a>
                     @endforeach
