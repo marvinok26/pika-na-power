e39d9da60631a23f6b97c85689745bcdd2787a5d
Makena-Maryann <maryann.makena00@gmail.com>
2024-08-15 10:48:46 +0300

    Bug fix

--- resources/views/livewire/sections/our-partners.blade.php
+++ resources/views/livewire/sections/our-partners.blade.php
@@ -14,7 +14,7 @@
             <div class="owl-carousel owl-carousel-2 owl-theme mt-5">
                 @foreach ($partners as $partner)
                     <div class="item">
-                        <a href={{ $shop->url }} target="_blank" rel="noreferrer" class="border-2 rounded-xl border-sky-300 dark:border-black bg-gray-50 p-6 text-center hover:border-blue-800">
+                        <a href={{ $partner->url }} target="_blank" rel="noreferrer" class="border-2 rounded-xl border-sky-300 dark:border-black bg-gray-50 p-6 text-center hover:border-blue-800">
                             <img data-aos="flip-right" src={{ url('storage/' . $partner->logo) }} class="h-28 object-contain" alt="">
                         </a>
                     </div>
