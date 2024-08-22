5c3b176f8534f2edf11dd4d57dd76ab42693251b
WanyoikeJ <jhnwanyoike@gmail.com>
2024-08-22 05:35:07 +0300

    Removed static images

--- resources/views/livewire/sections/banner.blade.php
+++ resources/views/livewire/sections/banner.blade.php
@@ -20,33 +20,6 @@
                                 <img src="{{ url('storage/' . $item->image) }}" class="h-full w-full object-cover">
                             </div>
                         @endforeach
-                        {{-- <!-- img tile two-->
-                        <div class="item">
-                            <div class="absolute left-9 z-50 inset-auto lg:px-0 py-16 sm:py-24 lg:pt-52">
-                                <div class="mx-auto w-full container h-full pb-20 md:pb-40">
-                                <!-- Newsroom Heading -->
-                                <h1 class="text-4xl font-serif lg:text-6xl my-6 max-w-3xl font-medium text-white lg:py-0 py-5 ">
-                                    {{$sectiondata->title}}
-                                </h1>
-                                <div class="text-white max-w-2xl pb-5">{!!$sectiondata->description!!}</div></div>
-                            </div>
-                            <div class="absolute inset-0 bg-black opacity-40"></div>
-                            <img src="https://pikapower.belva.co.ke/storage/01J3JH3T18QN6MFJM8F1WSQCWM.webp" class="h-full w-full object-cover">
-                        </div>
-                        <!-- img tile three-->
-                        <div class="item">
-                            <div class="absolute left-9 z-50 inset-auto lg:px-0 py-16 sm:py-24 lg:pt-52">
-                                <div class="mx-auto w-full container h-full pb-20 md:pb-40">
-                                <!-- Newsroom Heading -->
-                                <h1 class="text-4xl font-serif lg:text-6xl my-6 max-w-3xl font-medium text-white lg:py-0 py-5 ">
-                                    {{$sectiondata->title}}
-                                </h1>
-                                <div class="text-white max-w-2xl pb-5">{!!$sectiondata->description!!}</div>
-                            </div>
-                            </div>
-                            <div class="absolute inset-0 bg-black opacity-40"></div>
-                            <img src="https://pikapower.belva.co.ke/storage/01J3JH3T18QN6MFJM8F1WSQCWM.webp" class="h-full w-full object-cover">
-                        </div> --}}
                     </div>
                 </section>
             </div>
